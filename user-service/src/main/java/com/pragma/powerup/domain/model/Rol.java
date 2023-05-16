428b4c14498c7f0e72f4792c92044140bd1c4f04
andresdino <andresdino9ster@gmail.com>
2023-05-16 18:40:05 -0500

    Cargando los modelos de User y Rol

--- user-service/src/main/java/com/pragma/powerup/domain/model/Rol.java
+++ user-service/src/main/java/com/pragma/powerup/domain/model/Rol.java
@@ -1,4 +1,18 @@
 package com.pragma.powerup.domain.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rol {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
 }
