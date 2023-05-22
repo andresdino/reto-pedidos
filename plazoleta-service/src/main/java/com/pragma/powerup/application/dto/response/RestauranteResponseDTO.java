e1f3d028123d42c184958b6720ee982a63f5e24e
andresdino <andresdino9ster@gmail.com>
2023-05-22 13:50:02 -0500

    Cargando la capa Aplication  de Restaurante

--- plazoleta-service/src/main/java/com/pragma/powerup/application/dto/response/RestauranteResponseDTO.java
+++ plazoleta-service/src/main/java/com/pragma/powerup/application/dto/response/RestauranteResponseDTO.java
@@ -1,4 +1,18 @@
 package com.pragma.powerup.application.dto.response;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RestauranteResponseDTO {
+
+    private Long id;
+    private String nombre;
+    private String nit;
+    private String direccion;
+    private String telefono;
+    private String urlLogo;
+    private Long idPropietario;
 }
