0dc15f924630810cce74f69e3c21f3d5ff662c6a
mcruffel <hector.sanchezt@pragma.com.co>
2023-06-05 11:04:11 -0500

    05/06/2023 Pruebas unitarias de la de la 4 y la 7

--- plazoleta-service/src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/ObjectJpaAdapter.java
+++ plazoleta-service/src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/ObjectJpaAdapter.java
@@ -31,4 +31,6 @@ public class ObjectJpaAdapter implements IObjectPersistencePort {
         }
         return objectEntityMapper.toObjectModelList(entityList);
     }
+
+
 }
--- plazoleta-service/src/test/java/com/pragma/powerup/domain/usecase/PlatoUseCaseTest.java
+++ plazoleta-service/src/test/java/com/pragma/powerup/domain/usecase/PlatoUseCaseTest.java
@@ -1,17 +1,20 @@
 package com.pragma.powerup.domain.usecase;
 
+import com.pragma.powerup.application.dto.request.PlatoRequestDTO;
 import com.pragma.powerup.domain.api.IPlatoServicePort;
 import com.pragma.powerup.domain.model.Categoria;
 import com.pragma.powerup.domain.model.Plato;
 import com.pragma.powerup.domain.model.Restaurant;
 import com.pragma.powerup.domain.spi.persistence.IPlatoPersistencePort;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +27,10 @@ class PlatoUseCaseTest {
     IPlatoPersistencePort iPlatoPersistencePortMock;
 
     @InjectMocks
-    Plato plato = new Plato();
+    Plato dishCurrent = new Plato();
+
+    @InjectMocks
+    Plato modifyPlate = new Plato();
     @InjectMocks
     Categoria categoria;
 
@@ -38,19 +44,30 @@ class PlatoUseCaseTest {
 
     @BeforeEach
     void setUp() {
-        plato.setNombre("Bandeja paisa");
-        plato.setPrecio("20000");
-        plato.setDescripcion("bandeja con sop y jugo");
-        plato.setUrlImagen("");
-        plato.setActivo(true);
-        plato.setRestauranteId(restaurant);
-        plato.setCategoriaId(categoria);
-        Mockito.when(iPlatoPersistencePortMock.getPlatoById(1L)).thenReturn(plato);
+        dishCurrent.setId(1L);
+        dishCurrent.setNombre("Bandeja paisa");
+        dishCurrent.setPrecio("20000");
+        dishCurrent.setDescripcion("bandeja con sop y jugo");
+        dishCurrent.setUrlImagen("");
+        dishCurrent.setActivo(true);
+        dishCurrent.setRestauranteId(restaurant);
+        dishCurrent.setCategoriaId(categoria);
+        Mockito.when(iPlatoPersistencePortMock.getPlatoById(1L)).thenReturn(dishCurrent);
 
         List<Plato> clasPlatoList = new ArrayList<>();
 
-        clasPlatoList.add(plato);
+        clasPlatoList.add(dishCurrent);
         Mockito.when(iPlatoPersistencePortMock.getAllPlato()).thenReturn(clasPlatoList);
+        platoUseCase.putPlato(1L, dishCurrent);
+
+/*
+        modifyPlate.setNombre("Plato nuevo");
+        modifyPlate.setPrecio("30000");
+        modifyPlate.setDescripcion("Sin sopa");
+
+        Mockito.when(iPlatoPersistencePortMock.getPlatoById(dishCurrent.getId())).thenReturn(modifyPlate);
+*/
+
     }
 
     @AfterEach
@@ -63,6 +80,21 @@ class PlatoUseCaseTest {
     }
 
     @Test
-    void getAllPlatos() {
+    void getAllPlatos() {var data = platoUseCase.getAllPlatos();
     }
+
+    @Test
+    void putPlato(){
+
+        var data =  platoUseCase.putPlato(1L, dishCurrent);
+        Assertions.assertEquals("bandeja con sop y jugo", dishCurrent.getDescripcion());
+
+    }
+
+    @Test
+    void putEnableDisablePlato(){
+
+       platoUseCase.putEnableDisablePlato(1L, 1L);
+    }
+
 }
